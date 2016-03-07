7463dd0f4917747f4e09b78a9a9be4240c8e167c
nvancasaa <NVancas@autoalert.com>
2016-03-07 14:56:08 -0800

    Project Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication
+{
+    /// <summary>
+    /// The purpose of this exercise is to test your ability in various aspects particularly in C#.NET
+    /// Complete the tasks below and email your results to neilan@autoalert.com
+    /// 
+    /// 1. Parse and extract the data from the .json file included in this project
+    /// 2. Summarize the average payment for each model year and output the results
+    /// 3. Compare the average difference in payments for 2015 and 2016 models and output the results
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
